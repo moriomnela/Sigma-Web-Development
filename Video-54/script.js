21bfc1b1f65e9226fcc08ca822c6a250fd1747cc
moriomnela <moriomislamnela23@gmail.com>
2026-02-14 11:39:38 +0600

    Introduction to JavaScript

---
+++ Video-54/script.js
@@ -0,0 +1,16 @@
+alert("Are you ready");
+console.log("Hello Nela")
+console.log("Hello Nela")
+console.log("Hello Nela")
+
+var a = prompt("Enter Your Number")
+console.log("Your number is " +a)
+
+var isTrue = confirm("Are you leave this page")
+
+if(isTrue){
+    console.log("I Want leave this page")
+}else{
+    console.log("I don't want to leave this")
+}
+document.title = "Video-54"
