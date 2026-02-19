2459f9e5df167c1ebe994c9a1c7ace83e8da26d8
moriomnela <moriomislamnela23@gmail.com>
2026-02-19 23:19:42 +0600

    if, else if, else ladder

--- Video-56/index.js
+++ Video-56/index.js
@@ -1,4 +1,21 @@
-console.log("I am Moriom islam nela")
-console.log("I am Rubayet Rifat")
-console.log("I am Nurmoni")
-console.log("I am Nurmoni")
+// console.log("I am Moriom islam nela")
+// console.log("I am Rubayet Rifat")
+// console.log("I am Nurmoni")
+// console.log("I am Nurmoni")
+
+
+let age = 9;
+// let grace = 5;
+
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age % grace)
+// console.log(age * grace)
+// console.log(age / grace)
+
+
+if(age != 18){
+    console.log("You can give vote")
+}else{
+    console.log("You cannot give vote")
+}
---
+++ Video-56/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+console.log("I am Moriom islam nela")
+// console.log("I am Rubayet Rifat")
+// console.log("I am Nurmoni")
+// console.log("I am Nurmoni")
