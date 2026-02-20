1eab39d2362b76c001756056d7c8f4785c479b41
moriomnela <moriomislamnela23@gmail.com>
2026-02-20 10:06:30 +0600

    Loop

---
+++ Video-57/index.js
@@ -0,0 +1,41 @@
+//Fpr Loop
+
+// let a=1;
+
+// for (let i = 0; i <= 100; i++) {
+//    console.log(a+i);   
+// }
+
+//For in Loop
+// let obj = {
+//     name : "Nela",
+//     role : "Progammar",
+//     company : "Sigma" 
+// }
+// for (const key in obj) {
+//    console.log(key) 
+// }
+
+//For of loop
+
+// for (const c of "Nela") {
+//     console.log(c)
+// }
+
+//While-Loop
+
+// let i =1;
+// while(i <= 10){
+//     console.log(i);
+//     i++;
+// }
+
+//Do-While Loop
+
+let i= 10;
+do{
+    console.log(i);
+    i++;
+}
+while(i < 10){
+}
