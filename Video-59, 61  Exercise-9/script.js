fba4141bcdb6f53332f2be777c51e43076eaf745
moriomnela <moriomislamnela23@gmail.com>
2026-02-22 15:49:41 +0600

    Faulty Calculator

---
+++ Video-59, 61  Exercise-9/script.js
@@ -0,0 +1,22 @@
+
+console.log("Sum of two number")
+
+let a = 4;
+let b = 6;
+let c = "ssfsd"
+var s = true;
+var x= null;
+var y = undefined;
+
+
+console.log(a+b)
+console.log(typeof a,typeof b,typeof c,typeof s,typeof x,typeof y);
+
+
+let object = {
+    name : "Moriom",
+    "job code" :1843
+}
+console.log(object)
+object.salary = "50k";
+console.log(object)
---
+++ Video-60/index.js
@@ -0,0 +1,36 @@
+//Javascript Function
+
+// function nela(name){
+//     console.log("Hi "+ name +" You are beautyfull")
+//     console.log("Hi "+ name +" You are nice")
+// }
+// nela("Moriom")
+
+
+// function sum(a,b){
+//     console.log(a+b)
+//     console.log(a-b)
+//     console.log(a*b)
+//     console.log(a/b)
+// }
+// sum(15,5)
+
+// function sum(a,b){
+//     return(a+b)
+// }
+
+// result1 = sum(10,2)
+// result2 = sum(10,2)
+// result3 = sum(10,2)
+// result4 = sum(10,2)
+
+// console.log("These sum is",result1)
+// console.log("These substruction is",result2)
+// console.log("These Multiplication is",result3)
+// console.log("These Division is",result4)
+
+
+const func1 =(x) =>{
+    console.log("Number is ",x)
+} 
+func1(22)
