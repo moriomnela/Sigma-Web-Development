940d3f0539a139bc01e5a54e3d10a2bb6c87ae50
moriomnela <moriomislamnela23@gmail.com>
2026-02-23 23:36:25 +0600

    Javascript String

--- Video-60/index.js
+++ Video-60/index.js
@@ -7,4 +7,14 @@
  console.log(a[1])
  console.log(a[2])
  console.log(a[3])
+ console.log(a.length)
 
+let real_name = "Nela"
+let brother = "Rifat"
+
+console.log(`My name is  ${real_name} and my brother name is ${brother}`)
+
+let sister = "Nurmoni"
+console.log(sister.toLocaleUpperCase())
+console.log(sister.toLocaleLowerCase())
+console.log(sister.slice(2,7))
