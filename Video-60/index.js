c67abf89e3b0cec1c1d7c60f093195117f8d8d0d
moriomnela <moriomislamnela23@gmail.com>
2026-02-23 22:55:33 +0600

    Javascript String

--- Video-60/index.js
+++ Video-60/index.js
@@ -1,36 +1,10 @@
-//Javascript Function
-
-// function nela(name){
-//     console.log("Hi "+ name +" You are beautyfull")
-//     console.log("Hi "+ name +" You are nice")
-// }
-// nela("Moriom")
 
 
-// function sum(a,b){
-//     console.log(a+b)
-//     console.log(a-b)
-//     console.log(a*b)
-//     console.log(a/b)
-// }
-// sum(15,5)
+ console.log("This is string Tiotorial")
 
-// function sum(a,b){
-//     return(a+b)
-// }
+ let a = "Nela"
+ console.log(a[0])
+ console.log(a[1])
+ console.log(a[2])
+ console.log(a[3])
 
-// result1 = sum(10,2)
-// result2 = sum(10,2)
-// result3 = sum(10,2)
-// result4 = sum(10,2)
-
-// console.log("These sum is",result1)
-// console.log("These substruction is",result2)
-// console.log("These Multiplication is",result3)
-// console.log("These Division is",result4)
-
-
-const func1 =(x) =>{
-    console.log("Number is ",x)
-} 
-func1(22)
